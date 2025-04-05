18021746b414bbb36864ed534746124842438415
eucristiancabral <cristianjosecabral@outlook.com>
2025-04-05 12:23:42 -0300

    feat(struct): creates struct Client to store name and email
    Defines the 'Client' structure with the fields 'name' and 'email', both with a maximum size of 100 characters.
    
    This struct will be used to store and manipulate customer data throughout the program, making the code more organized and data-oriented.

--- CustomerRegistration.c
+++ CustomerRegistration.c
@@ -2,3 +2,8 @@
 #include <stdlib.h>    // Standard library
 #include <string.h>    // String manipulation functions
 
+// Structure to store client data
+struct Client {
+    char name[100];
+    char email[100];
+};
